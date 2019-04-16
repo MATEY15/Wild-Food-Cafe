17c6862b4f5c044675de70a0d3b3474d16398af4
MATEY15 <xzzmanzzx@gmail.com>
2019-04-17 02:49:34 +0300

    New version

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,5 +1,7 @@
 //= jquery.min.js
 //= jquery.magnific-popup.min.js
+//= PageScroll2id.min.js
+//= owl.carousel.min.js
 
 // window.onload = function(){
 //   // $("body").toggleClass('opacity');
@@ -11,11 +13,20 @@
 // }
 
 $(".burger-menu").on("click", function() {  
+  $(this).toggleClass('is-active');
+  $(".top-line").toggleClass('is-active');
   $(".menu-header").toggleClass('is-active');
   $("body").toggleClass('overflow');
 })
 
-/* Popup Window */
+function heightFooter(){
+  var heightFoot = $('.section--footer').outerHeight();
+  $('body').css({ 'padding-bottom': heightFoot});
+}
+heightFooter()
+$( window ).resize(function() {
+  heightFooter()
+});
 
 $(".popup").magnificPopup({
   type: 'inline',
@@ -23,4 +34,42 @@ $(".popup").magnificPopup({
   mainClass: 'my-mfp-slide-bottom'
 });
 
-/* Popup Window End */
+$(".scroll-section").mPageScroll2id();
+
+$('.slider-events').owlCarousel({
+	loop: false,
+	margin: 30,
+	nav:true,
+	responsive:{
+		0:{
+			items:1
+		},
+		721:{
+			items:2
+		},
+		1000:{
+			items:3
+		}
+	}
+});
+$('.recipes-slide').owlCarousel({
+	loop: false,
+	margin: 40,
+	nav:true,
+	responsive:{
+		0:{
+			items: 1
+		},
+		560:{
+			items:2
+		},
+		855:{
+			items:3,
+			margin: 30
+		},
+		1025:{
+			items:4,
+			margin: 40
+		}
+	}
+});
